ac0005a5e530e8a9fd44e3696f144d5974d6e7b0
P4blit0 <radicpaul25@gmail.com>
2019-09-16 01:03:50 +0200

    linking bootstrap in index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>My Youtube To MP3</title>
+    <link rel="stylesheet" href="/css/bootstrap.css">
   </head>
   <body>
     <form class="" action="index.html" method="post">
