6494750a9de23f2fd6523c3825cdf5b7feb54aca
P4blit0 <radicpaul25@gmail.com>
2019-09-16 01:14:19 +0200

    linking style.css in index.php and creating form using bootstrap

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
   <head>
     <meta charset="utf-8">
     <title>My Youtube To MP3</title>
-    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
     <form class="" action="index.html" method="post">
-      <input type="text" name="link" value="link">
-      <input type="submit" name="" value="">
+      <div class="form-group">
+        <input type="text" class="form-control" id="linkInput" name="link" value="link">
+        <input type="submit" class="btn btn-primary" id="downloadBtn" name="download_btn" value="Download">
+      </div>
     </form>
   </body>
 </html>
