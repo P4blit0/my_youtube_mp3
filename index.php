eb634256f7d053529a53a71b4f7d7380242beb71
P4blit0 <radicpaul25@gmail.com>
2019-09-16 00:58:43 +0200

    creating html structure in index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php echo "test"; ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My Youtube To MP3</title>
+  </head>
+  <body>
+    <form class="" action="index.html" method="post">
+      <input type="text" name="link" value="link">
+      <input type="submit" name="" value="">
+    </form>
+  </body>
+</html>
